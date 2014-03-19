fc553d26a14f24165fc83eee33935451d00cd24b
Brandon Morreale <brandon.morreale@gmail.com>
2014-03-18 21:31:12 -0500

    Analytics

--- scripts.php
+++ scripts.php
@@ -11,4 +11,20 @@
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
     <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
+    <script type="text/javascript">
+	function woopraReady(tracker) {
+	    tracker.setDomain('brandonmorreale.com');
+	    tracker.setIdleTimeout(300000);
+	    tracker.track();
+	    return false;
+	}
+	(function() {
+	    var wsc = document.createElement('script');
+	    wsc.src = document.location.protocol+'//static.woopra.com/js/woopra.js';
+	    wsc.type = 'text/javascript';
+	    wsc.async = true;
+	    var ssc = document.getElementsByTagName('script')[0];
+	    ssc.parentNode.insertBefore(wsc, ssc);
+	})();
+	</script>
 </head>
